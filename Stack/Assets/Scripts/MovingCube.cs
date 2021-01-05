be93bd1b2a6a4ff3c758cd27082ab141dacfc6e7
AndreiMercieca <74543545+AndreiMercieca@users.noreply.github.com>
2021-01-05 20:55:11 +0100

    Created the cube spawner

--- Stack/Assets/Scripts/MovingCube.cs
+++ Stack/Assets/Scripts/MovingCube.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 
 public class MovingCube : MonoBehaviour
 {
-    public static MovingCube CurrentCube {get; private set;}
-        public static MovingCube LastCube {get; private set;}
-    [SerializeField]
+    public static MovingCube CurrentCube {get; private set; }
+    public static MovingCube LastCube {get; private set; }
+
+[SerializeField]
     private float moveSpeed = 1f;
 
     private void OnEnable()
     {
         if(LastCube == null)
         LastCube = GameObject.Find("Start").GetComponent<MovingCube>();
-        CurrentCube =this;
+
+        CurrentCube = this;
         GetComponent<Renderer>().material.color=GetRandomColor();
+
+        transform.localScale = new Vector3(LastCube.transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 
     private Color GetRandomColor()
@@ -27,39 +31,41 @@ public class MovingCube : MonoBehaviour
     {
         moveSpeed = 0;
         float hangover = transform.position.z - LastCube.transform.position.z;
+
         float direction = hangover > 0 ? 1f : -1f;
-        SplitCubeOnZ(hangover,direction);
-    
+        SplitCubeOnZ(hangover, direction);
     }
 
-    private void SplitCubeOnZ(float hangover,float direction)
+        private void SplitCubeOnZ(float hangover, float direction)
     {
         float newZsize = LastCube.transform.localScale.z - Mathf.Abs(hangover);
-        float fallinglockSize = transform.localScale.z - newZsize;
+        float fallingBlockSize = transform.localScale.z - newZsize;
     
-    float newZPosition = LastCube.transform.position.z + (hangover / 2);
+        float newZPosition = LastCube.transform.position.z + (hangover / 2);
     transform.localScale = new Vector3( transform.localScale.x,transform.localScale.y, newZsize);
     transform.position = new Vector3(transform.position.x,transform.position.y,newZPosition);
 
-    float cubeEdge = transform.position.z + (newZsize/2f * direction);
-    float fallingBlockZposition = cubeEdge + fallinglockSize / 2f * direction; 
-    
-    SpawnDropCube(fallingBlockZposition,fallinglockSize);
-    
+    float cubeEdge = transform.position.z + (newZsize/ 2f * direction);
+        float fallingBlockZposition = cubeEdge + fallingBlockSize / 2f * direction;
+
+    SpawnDropCube(fallingBlockZposition, fallingBlockSize);
     }
 
-    private void SpawnDropCube(float fallingBlockZposition,float fallinglockSize)
+        private void SpawnDropCube(float fallingBlockZposition, float fallingBlockSize)
     {
         var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.localScale = new Vector3(transform.localScale.x,transform.localScale.y,fallinglockSize);
+        cube.transform.localScale = new Vector3(transform.localScale.x,transform.localScale.y,fallingBlockSize);
         cube.transform.position = new Vector3(transform.position.x,transform.position.y,fallingBlockZposition);
 
         cube.AddComponent<Rigidbody>();
-        GetComponent<Renderer>().material.color = GetComponent<Renderer>().material.color;
+        cube.GetComponent<Renderer>().material.color = cube.GetComponent<Renderer>().material.color;
         Destroy(cube.gameObject, 1f);
+
+
+
     }
 
-    private void Update()
+    void Update()
     {
         transform.position += transform.forward * Time.deltaTime * moveSpeed;
     }
